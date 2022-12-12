aec824078919e44e9c2467e46b05b7c3e899b28d
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-12 15:57:26 +0100

    Info page styling, divider added

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,42 +1,33 @@
-// Contains general info 
+// Contains general info
+//TODO: Contact e-mail should be a button with mailto
+//TODO: Health inspection link should be a button
 
-import React from 'react'
+import React from "react";
+import Divider from "../assets/divider.svg";
+import './Info.css';
 
 export default function Info() {
   return (
-    <div>
-    ADDRESS
-    
-    FREDENSGADE 41
-    
-     8000 AARHUS
-    
-    -
-    FIND US
-    
-    BACKYARD - GREEN DOOR
-    
-    _
-    OPEN
-    
-    THURSDAY 
-    
-    18.00 - 00:00
-    
-    FRIDAY & SATURDAY  
-    
-    16.00 - 02:00
-    
-    —
-    CONTACT
-    
-    AARHUS@GEDULGT.DK
-    
-    —
-    FURTHER INFORMATION
-    HEALTH INSPECTION REPORT
-    
-    —
+    <div className="info">
+      <p>ADDRESS</p>
+      <p>FREDENSGADE 41</p>
+      <p>8000 AARHUS</p>
+      <img className="divider" src={Divider}/>
+      <p>FIND US</p>
+      <p>BACKYARD - GREEN DOOR</p>
+      <img className="divider" src={Divider}/>
+      <p>OPEN</p>
+      <p>THURSDAY</p>
+      <p>18.00 - 00:00</p>
+      <p>FRIDAY & SATURDAY</p>
+      <p>16.00 - 02:00</p>
+      <img className="divider" src={Divider}/>
+      <p>CONTACT</p>
+      <p>AARHUS@GEDULGT.DK</p>
+      <img className="divider" src={Divider}/>
+      <p>FURTHER INFORMATION</p>
+      <a href="https://findsmiley.dk/962310">HEALTH INSPECTION REPORT</a>
+      <img className="divider" src={Divider}/>
     </div>
-  )
+  );
 }
