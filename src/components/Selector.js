963d9023e20eb08fb6c56edef7368d882fd159bc
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-13 23:21:54 +0100

    Selector basic structure

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,1 +1,18 @@
 // The selector button on the products page, which allows the user to switch around different menus
+
+import React from "react";
+import "./Selector.css";
+import LeftArrow from "../assets/left_arrow.svg";
+import RightArrow from "../assets/right_arrow.svg";
+
+export default function Selector({ current_menu }) {
+  return (
+    <div className="selector-container">
+        <div className="selector">
+            <img className="leftarrow" src={LeftArrow} />
+                <p className="selector_text">{current_menu}</p>
+            <img className="rightarrow" src={RightArrow} />
+        </div>
+    </div>
+  );
+}
