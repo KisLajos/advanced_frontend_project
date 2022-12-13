91bbd23fb933baadedca0206edd296e04c7fdb9f
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-13 23:21:38 +0100

    Navigation icons basic structure

--- src/components/NavIcons.js
+++ src/components/NavIcons.js
@@ -1,1 +1,19 @@
 // The small navigation icons on the menu subpages
+
+import React from "react";
+import "./NavIcons.css";
+import SnacksIcon from "../assets/icon_snacks.svg"
+import CocktailsIcon from "../assets/icon_cocktails.svg"
+import OtherdrinksIcon from "../assets/icon_otherdrinks.svg"
+import MerchIcon from "../assets/icon_merch.svg"
+
+export default function NavIcons() {
+  return (
+    <div className="navicons">
+      <img src={SnacksIcon}/>
+      <img src={CocktailsIcon}/>
+      <img src={OtherdrinksIcon}/>
+      <img src={MerchIcon}/>
+    </div>
+  );
+}
