03d8337c689c5967573f3cce3353126deb2ebdae
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-14 01:36:18 +0100

    Added secrets page

---
+++ src/pages/Secrets.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import Product from "../components/Product.js";
+import Selector from "../components/Selector.js";
+import NavIcons from "../components/NavIcons.js";
+
+export default function Secrets() {
+  const [products, setProducts] = useState([]);
+  //const current_menu_name = "CHRISTMAS MENU";
+  const current_menu_name = "THE BEGINNING OF THE WORLD";
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        "https://wordpress.lajoskis.dk/wp-json/wp/v2/products?_embed"
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data.data?.status !== 404) {
+        setProducts(data);
+      }
+    }
+    getData();
+  }, []);
+
+  return (
+    <section className="page">
+      <NavIcons/>
+      <Selector current_menu={ current_menu_name }/>
+      {products?.length > 0 ? products.map((product) => (
+            <Product key={product.id} product={product} />
+          )) : "Loading..."}
+    </section>
+  );
+}
