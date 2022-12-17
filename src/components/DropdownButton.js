1a4a62bd330ac4f36299ba766c4bd4237ed034b2
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-17 16:31:08 +0100

    Dropdown buttons built from data

--- src/components/DropdownButton.js
+++ src/components/DropdownButton.js
@@ -1,21 +1,60 @@
 // The dropdown menus on the menu page
 
-import React from "react";
+import React, { useState } from "react";
 import DownArrow from "../assets/down_arrow.svg";
-import './MenuButtons.css';
-import './DropdownButton.css';
+import DropdownSubbutton from "./DropdownSubbutton.js";
+import parse from "html-react-parser";
+import "./MenuButtons.css";
+import "./DropdownButton.css";
 
 export default function DropdownButton( props ) {
-  return (
+  const [open, setOpen] = useState(false);
+  const categories = props.categories.categories;
+  const current_category = props.type;
+  const [showSubbutton, setShowSubbutton] = useState(false);
+  const [className, setClassName] = useState("");
 
-    <button className="button-container dropdown-button">
-      <div className="grid-icon">
-        <img src={ props.imgsrc } />
-      </div>
-      <div className="grid-text"> { props.text } </div>
-      <div className="grid-arrow">
-      <img alt="Down arrow" src={DownArrow} />        
-      </div>
-    </button>
+  const handleOpen = () => {
+    setOpen(!open);
+    if (open) {
+      setShowSubbutton(open);
+    }
+
+    setClassName(open ? "openAnim" : "closeAnim");
+  };
+
+  return (
+    <>
+      <button onClick={handleOpen} className="button-container dropdown-button">
+        <div className="grid-icon">
+          <img src={props.imgsrc} />
+        </div>
+        <div className="grid-text"> {props.text} </div>
+        <div className="grid-arrow">
+          <img alt="Down arrow" src={DownArrow} />
+        </div>
+      </button>
+      {open ? (
+        <div className="dropdown-content">
+          {console.log(categories)}
+          {categories?.length > 0
+            ? categories.map((category) =>
+                category.slug.split("-")[0] === current_category ? (
+                  <DropdownSubbutton
+                    className={className}
+                    onClose={setShowSubbutton}
+                    key={category.id}
+                    linksto={
+                      "/products/" +
+                      category.slug.substring(category.slug.indexOf("-") + 1)
+                    }
+                    text={parse(category.name)}
+                  />
+                ) : null
+              )
+            : "Loading..."}
+        </div>
+      ) : null}
+    </>
   );
 }
