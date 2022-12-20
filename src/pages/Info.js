a9dd7a5ca78b0c7046424dc7313f61db2f362aca
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-12-20 11:55:18 +0100

    Info page fixes

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,14 +1,17 @@
 // Contains general info
-//TODO: Contact e-mail should be a button with mailto
-//TODO: Health inspection link should be a button
 
 import React from "react";
 import Divider from "../assets/divider.svg";
+import Logo from "../assets/logo_transparent.png";
+import Footer from "../components/Footer.js";
 import './Info.css';
 
 export default function Info() {
   return (
     <div className="info">
+      <div className="logo-container">
+        <img src={Logo} alt="Gedulgt" className="logo" />
+      </div>
       <p>ADDRESS</p>
       <p>FREDENSGADE 41</p>
       <p>8000 AARHUS</p>
@@ -23,11 +26,11 @@ export default function Info() {
       <p>16.00 - 02:00</p>
       <img className="divider" src={Divider}/>
       <p>CONTACT</p>
-      <p>AARHUS@GEDULGT.DK</p>
+      <a className="info-button" href="mailto:aarhus@gedulgt.dk">AARHUS@GEDULGT.DK</a>
       <img className="divider" src={Divider}/>
       <p>FURTHER INFORMATION</p>
-      <a href="https://findsmiley.dk/962310">HEALTH INSPECTION REPORT</a>
-      <img className="divider" src={Divider}/>
+      <a className="info-button" href="https://findsmiley.dk/962310">HEALTH INSPECTION REPORT</a>
+      <Footer/>
     </div>
   );
 }
